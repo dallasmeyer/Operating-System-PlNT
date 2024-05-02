6e96ee216d6ca49f7eb75e0247b105dd2813e0ae
Keith Irby <kirby@ucsc.edu>
2024-05-02 20:12:01 +0000

    Adding thread_start() to init.c so the thread pull begins

--- src/threads/init.c
+++ src/threads/init.c
@@ -298,6 +298,8 @@ run_task (char **argv)
   printf ("Executing '%s':\n", task);
 #ifdef USERPROG
   process_wait (process_execute (task));
+  // NEW: start the thread pool
+  thread_start();
 #else
   run_test (task);
 #endif
